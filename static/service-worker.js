076b21f5f0dc59f6135b2ae64b8416b184e51a2d
Prollad Mondal <prolladmail@gmail.com>
2024-07-24 12:44:49 +0530

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,36 +1,22 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('app-shell').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/css/main.css',
-                '/favicon.ico',
-                '/manifest.json',
-                '/icons/icon-192x192.png',
-                '/icons/icon-512x512.png'
-            ]);
-        })
-    );
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('static-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/styles.css',
+        '/manifest.json',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-self.addEventListener('activate', event => {
-    var cacheWhitelist = ['app-shell'];
-    event.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(keyList.map(key => {
-                if (cacheWhitelist.indexOf(key) === -1) {
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
