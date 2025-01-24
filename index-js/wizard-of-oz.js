07cb00df41d614103979c1638e6c93458708ca7b
Prollad Mondal <prolladmail@gmail.com>
2025-01-24 16:13:27 +0530

    Update wizard-of-oz.js

--- index-js/wizard-of-oz.js
+++ index-js/wizard-of-oz.js
@@ -3,9 +3,19 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
 
+// Function to get the current date in MM-DD-YYYY format
+function getCurrentDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${month}-${day}-${year}`;
+}
+
 const url = 'https://mosttechs.com/wizard-of-oz-slots-free-coins/';
 
-axios.get(url)
+axios
+  .get(url)
   .then(({ data }) => {
     const $ = cheerio.load(data);
     const links = [];
@@ -13,13 +23,14 @@ axios.get(url)
     $('a[href*="zdnwoz0-a.akamaihd.net"], a[href*="zynga.social"]').each((index, element) => {
       const link = $(element).attr('href');
       const text = $(element).text().trim();
-      links.push({ href: link, text: text });
+      const dateText = `Wizard of Oz Coins ${getCurrentDate()}`;
+      links.push({ href: link, text: dateText, button: `<a href="${link}" class="btn btn-primary">Collect</a>` });
     });
 
     console.log('Fetched links:', links);
 
     const dir = 'links-json';
-    if (!fs.existsSync(dir)){
+    if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
 
@@ -27,7 +38,7 @@ axios.get(url)
     fs.writeFileSync(filePath, JSON.stringify(links, null, 2), 'utf8');
     console.log(`Links saved to ${filePath}`);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error('Error fetching links:', err);
     process.exit(1);
-  }); 
+  });
